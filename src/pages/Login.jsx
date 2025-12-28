fe536bec1edf08186084cc7cf55bdd7089880a17
asanka <asankasampath200228@gmail.com>
2025-12-28 15:26:20 +0530

    feat: Add UserContext for user state management and wrap Layout with UserProvider

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+
+const {userContext}  from
+
 const Login = () => {
     const [userData,setUserData] = useState({
         email: '',
